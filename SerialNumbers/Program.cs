526a12ea331b8f9475a53198ea3e0969cad94558
LashaMorgoshia <lashamor@gmail.com>
2022-05-18 18:12:59 +0400

    upload to git

---
+++ SerialNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+
+Console.Write("Input the folder path and press Enter to read serials: ");
+var path = Console.ReadLine();
+var files = Directory.GetFiles(path);
+
+StringBuilder sb = new StringBuilder();
+
+foreach (var file in files)
+{
+    if (!file.EndsWith("csv")) continue;
+
+    FileInfo fileInfo = new FileInfo(file);
+
+    var lines = File.ReadAllText(file);
+    Console.WriteLine(string.Join("\n", lines));
+
+    sb.AppendLine($"{fileInfo.Name} - {lines.Split("\r\n")[1]}");
+}
+
+File.WriteAllText($"{path}\\serials.txt", sb.ToString());
+
+Console.WriteLine("done");
