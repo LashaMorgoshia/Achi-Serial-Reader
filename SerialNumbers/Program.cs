16f89414b4156a29d90dc2381a97186fe78da3c9
LashaMorgoshia <lashamor@gmail.com>
2022-05-18 18:33:15 +0400

    Add try catch

--- SerialNumbers/Program.cs
+++ SerialNumbers/Program.cs
@@ -1,24 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 
-Console.Write("Input the folder path and press Enter to read serials: ");
-var path = Console.ReadLine();
-var files = Directory.GetFiles(path);
-
-StringBuilder sb = new StringBuilder();
-
-foreach (var file in files)
+try
 {
-    if (!file.EndsWith("csv")) continue;
 
-    FileInfo fileInfo = new FileInfo(file);
+    Console.Write("Input the folder path and press Enter to read serials: ");
+    var path = Console.ReadLine();
+    var files = Directory.GetFiles(path);
 
-    var lines = File.ReadAllText(file);
-    Console.WriteLine(string.Join("\n", lines));
+    StringBuilder sb = new StringBuilder();
+    FileInfo fileInfo;
 
-    sb.AppendLine($"{fileInfo.Name} - {lines.Split("\r\n")[1]}");
+    foreach (var file in files)
+    {
+        try
+        {
+            if (!file.EndsWith("csv")) continue;
+
+            fileInfo = new FileInfo(file);
+
+            var lines = File.ReadAllText(file);
+            var splitLines = lines.Split("\r\n");
+            Console.WriteLine($"{file} {string.Join("\n", lines)}");
+
+            if (splitLines.Length > 0)
+                sb.AppendLine($"{fileInfo.Name} - {splitLines[1]}");
+            else Console.WriteLine($"ERROR - {file}");
+        }
+        catch (Exception ex) { Console.WriteLine(ex.Message); }
+        
+    }
+
+    File.WriteAllText($"{path}\\serials.txt", sb.ToString());
 }
-
-File.WriteAllText($"{path}\\serials.txt", sb.ToString());
-
+catch(Exception ex) { Console.WriteLine(ex.Message); }
 Console.WriteLine("done");
